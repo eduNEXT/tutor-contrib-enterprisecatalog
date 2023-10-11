6e805140a89254f75bc30a36c8e5fabc6de5dabc
Erick Murillo <erickmurillo22@gmail.com>
2023-10-11 12:58:21 -0600

    feat: update dockerfile and other files

--- tutorenterprisecatalog/plugin.py
+++ tutorenterprisecatalog/plugin.py
@@ -15,7 +15,8 @@ config = {
     },
     "defaults": {
         "VERSION": __version__,
-        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}diceytech/openedx-enterprise-catalog:{{ ENTERPRISECATALOG_VERSION }}",
+        # "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}diceytech/openedx-enterprise-catalog:{{ ENTERPRISECATALOG_VERSION }}",
+        "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}erickmurillo/openedx-enterprise-catalog:latest",
         "WORKER_DOCKER_IMAGE": "openedx-enterprise-catalog-worker",
         "HOST": "enterprisecatalog.{{ LMS_HOST }}",
         "MYSQL_DATABASE": "enterprisecatalog",
